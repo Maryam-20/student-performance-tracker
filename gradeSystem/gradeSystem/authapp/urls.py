67776b8f8165b7326a31f2972ac04054616e52f5
Maryam Anileleye <maryamanileleye.bincom@gmail.com>
2025-05-17 01:26:50 +0100

    FEAT: Add profile page

--- gradeSystem/gradeSystem/authapp/urls.py
+++ gradeSystem/gradeSystem/authapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SignupView
+from .views import myProfileView
 
 
 urlpatterns = [
+    path('my_profile/<int:userid>/', myProfileView.as_view(), name='my_profile'),
     # path('signup/', SignupView.as_view(), name='signup'),
     # Add other URL patterns here as needed
 ]
--- gradeSystem/gradeSystem/authapp/views.py
+++ gradeSystem/gradeSystem/authapp/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LogoutView
 from django.urls import reverse_lazy
 # from django.views import View
 from django.contrib.auth import login, authenticate
 from .forms import SignUpForm
-from django.views import generic
+from django.views import generic, View
 from django.contrib.auth import get_user_model
+from .models import AllUser, Staff, Student, Teacher
 
 # Create your views here.
 
@@ -20,3 +22,13 @@ class SignupView(generic.CreateView):
 class CustomLogoutView(LogoutView):
     def get(self, request, *args, **kwargs):
         return self.post(request, *args, **kwargs)
+
+
+
+class myProfileView(LoginRequiredMixin, View):
+    def get(self, request, userid):
+        student = Student.objects.all().filter(user_id=userid)
+        staff = Staff.objects.all().filter(user_id=userid)
+        teacher = Teacher.objects.all().filter(staff_id=userid)
+        return render(request, 'authapp/my_profile.html', {'student': student, 'staff': staff, 'teacher': teacher})
+    
